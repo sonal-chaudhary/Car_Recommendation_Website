6978295421ab3908021c346821dd36355158fb98
Sonal Chaudhary <sonalchaudhary5213@gmail.com>
2025-11-02 23:34:15 +0530

    Delete car-backend directory

--- car-backend/models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  name: { type: String, default: "" },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true } // hashed
-}, { timestamps: true });
-
-export default mongoose.model("User", userSchema);
